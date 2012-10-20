b038c50873c4b4bd995009180b5f184753d70add
Can Margarini <Margarinic@gmail.com>
2012-10-20 11:34:26 -0400

    final changes
    fixed database connection to server

--- source/submitForm.php
+++ source/submitForm.php
@@ -9,8 +9,8 @@ require_once('../../sets.php');
 //select the DataBase
 mysql_select_db($emailDB)or die ('cannot select DB :('. mysql_error());
 
-$query="INSERT INTO `raffleiz_emails`.`emails` (`ID`, `emails`) VALUES (NULL, '".$UsrEmail."')" or die ('cannot select DB :('. mysql_error());
-mysql_query($query) or die ('Error Updating Data');
+$query="INSERT INTO `raffleiz_emails`.`emails` (`UsrID`, `UsrEmail`, `UsrName`, `UsrLastName`,	`UsrAge`, `UsrGender`) VALUES (NULL, '".$UsrEmail."', NULL, NULL, NULL, NULL)" or die ('cannot select DB :('. mysql_error());
+mysql_query($query) or die ('Error Updating Data'. mysql_error());
 
 //send email to user
 $to = $UsrEmail;
@@ -23,5 +23,16 @@ $to = $UsrEmail;
   }
 $_SESSION['ON-OFF'] = 1;
   
-header( 'Location: ../home.php' ) ;
+
 ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="REFRESH" content="0;url=../index.php">
+<title>Raffleize</title>
+</head>
+
+<body>
+</body>
+</html>
